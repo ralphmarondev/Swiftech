56c39ad474e00141b4ef633ea967e5e36ba14136
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-03-31 08:12:16 +0800

    set table_name of User to user

--- app/src/main/java/com/ralphmarondev/swiftech/core/domain/model/User.kt
+++ app/src/main/java/com/ralphmarondev/swiftech/core/domain/model/User.kt
@@ -3,13 +3,13 @@ package com.ralphmarondev.swiftech.core.domain.model
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "user")
 data class User(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
-    val role: String,
     val username: String,
     val password: String,
+    val role: String,
     val fullName: String? = null,
     val image: String? = null
 )
