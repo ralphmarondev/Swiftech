2163c9b3ca23b925c334d1e556abc5ec0315d2a9
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-04-04 04:42:07 +0800

    initial new course screen and viewmodel

--- app/src/main/java/com/ralphmarondev/swiftech/features/courses/presentation/new_course/NewCourseScreen.kt
+++ app/src/main/java/com/ralphmarondev/swiftech/features/courses/presentation/new_course/NewCourseScreen.kt
@@ -1,27 +1,80 @@
 package com.ralphmarondev.swiftech.features.courses.presentation.new_course
 
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AccountBox
 import androidx.compose.material.icons.outlined.ArrowBackIosNew
+import androidx.compose.material.icons.outlined.PersonOutline
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
+import com.ralphmarondev.swiftech.core.presentation.NormalTextField
+import com.ralphmarondev.swiftech.core.util.saveImageToAppFolder
+import org.koin.androidx.compose.koinViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NewCourseScreen(
     navigateBack: () -> Unit
 ) {
+    val viewModel: NewCourseViewModel = koinViewModel()
+    val name = viewModel.name.collectAsState().value
+    val code = viewModel.code.collectAsState().value
+    val teacher = viewModel.teacher.collectAsState().value
+    val imagePath = viewModel.imagePath.collectAsState().value
+    val response = viewModel.response.collectAsState().value
+
+    val context = LocalContext.current
+    val focusManager = LocalFocusManager.current
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        uri?.let {
+            val path = saveImageToAppFolder(context, it)
+            path?.let { image ->
+                viewModel.onImagePathChange(image)
+            }
+        }
+    }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -52,14 +105,130 @@ fun NewCourseScreen(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                text = "Add new courses",
-                fontWeight = MaterialTheme.typography.titleLarge.fontWeight,
-                fontSize = MaterialTheme.typography.titleLarge.fontSize,
-                color = MaterialTheme.colorScheme.secondary,
-                modifier = Modifier.padding(16.dp)
+            Spacer(modifier = Modifier.height(8.dp))
+            Image(
+                painter = rememberAsyncImagePainter(imagePath),
+                contentDescription = "Course image",
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(CircleShape),
+                contentScale = ContentScale.Crop
             )
+            Spacer(modifier = Modifier.height(4.dp))
+            TextButton(
+                onClick = {
+                    launcher.launch("image/*")
+                }
+            ) {
+                Text(
+                    text = "Change photo",
+                    fontSize = 14.sp,
+                    color = MaterialTheme.colorScheme.outline
+                )
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+            NormalTextField(
+                value = name,
+                onValueChange = viewModel::onFullNameChange,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp, vertical = 4.dp),
+                label = "Name",
+                placeholder = "Technopreneurship",
+                leadingIcon = Icons.Outlined.PersonOutline,
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Next
+                ),
+                keyboardActions = KeyboardActions(
+                    onNext = {
+                        focusManager.moveFocus(FocusDirection.Next)
+                    }
+                )
+            )
+
+            NormalTextField(
+                value = code,
+                onValueChange = viewModel::onUsernameChange,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp, vertical = 4.dp),
+                label = "Code",
+                placeholder = "T2025",
+                leadingIcon = Icons.Outlined.AccountBox,
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Next
+                ),
+                keyboardActions = KeyboardActions(
+                    onNext = {
+                        focusManager.moveFocus(FocusDirection.Next)
+                    }
+                )
+            )
+
+
+            Spacer(modifier = Modifier.height(2.dp))
+            AnimatedVisibility(response?.success == false) {
+                if (response?.message != null) {
+                    Text(
+                        text = response.message,
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.W500,
+                        color = if (response.success) MaterialTheme.colorScheme.secondary else MaterialTheme.colorScheme.error,
+                        maxLines = 2,
+                        overflow = TextOverflow.Ellipsis,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 8.dp, vertical = 16.dp)
+                    )
+                }
+            }
+            AnimatedVisibility(response?.success == true) {
+                if (response?.message != null) {
+                    Text(
+                        text = response.message,
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.W500,
+                        color = if (response.success) MaterialTheme.colorScheme.secondary else MaterialTheme.colorScheme.error,
+                        maxLines = 2,
+                        overflow = TextOverflow.Ellipsis,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 8.dp, vertical = 16.dp)
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Button(
+                onClick = viewModel::register,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                Text(
+                    text = "CREATE",
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.W500,
+                    fontSize = 16.sp
+                )
+            }
+            ElevatedButton(
+                onClick = viewModel::register,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                Text(
+                    text = "CREATE AND ADD STUDENT",
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.W500,
+                    fontSize = 16.sp
+                )
+            }
         }
     }
 }
--- app/src/main/java/com/ralphmarondev/swiftech/features/courses/presentation/new_course/NewCourseViewModel.kt
+++ app/src/main/java/com/ralphmarondev/swiftech/features/courses/presentation/new_course/NewCourseViewModel.kt
@@ -2,13 +2,57 @@ package com.ralphmarondev.swiftech.features.courses.presentation.new_course
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.ralphmarondev.swiftech.core.data.local.preferences.AppPreferences
+import com.ralphmarondev.swiftech.core.domain.model.Result
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
-class NewCourseViewModel : ViewModel() {
+class NewCourseViewModel(
+    private val preferences: AppPreferences
+) : ViewModel() {
+
+    private val defaultImage = preferences.getDefaultImage()
+
+    private val _name = MutableStateFlow("")
+    val name = _name.asStateFlow()
+
+    private val _code = MutableStateFlow("")
+    val code = _code.asStateFlow()
+
+    private val teacherId = MutableStateFlow(-1)
+    val teacher = teacherId.asStateFlow()
+
+    private val _imagePath = MutableStateFlow(defaultImage ?: "")
+    val imagePath = _imagePath.asStateFlow()
+
+    private val _response = MutableStateFlow<Result?>(null)
+    val response = _response.asStateFlow()
+
+
+    fun onFullNameChange(value: String) {
+        _name.value = value
+    }
+
+    fun onUsernameChange(value: String) {
+        _code.value = value
+    }
+
+    fun onPasswordChange(value: Int) {
+        teacherId.value = value
+    }
+
+    fun onImagePathChange(value: String) {
+        _imagePath.value = value
+    }
 
     init {
         viewModelScope.launch {
             println("New course viewmodel is initialized...")
         }
+    }
+
+    fun register() {
+
     }
 }
