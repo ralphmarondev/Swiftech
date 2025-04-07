1f98f8e1f00de913d522945dd04740e3a586a264
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-04-07 17:28:19 +0800

    added NewEvaluationViewModel and NewQuestionDialog

--- app/src/main/java/com/ralphmarondev/swiftech/features/evaluation/presentation/new_evaluation/NewEvaluationScreen.kt
+++ app/src/main/java/com/ralphmarondev/swiftech/features/evaluation/presentation/new_evaluation/NewEvaluationScreen.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material.icons.outlined.ArrowBackIosNew
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -14,14 +16,20 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.ralphmarondev.swiftech.features.evaluation.presentation.components.NewQuestionDialog
+import org.koin.androidx.compose.koinViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NewEvaluationScreen(
     navigateBack: () -> Unit
 ) {
+    val viewModel: NewEvaluationViewModel = koinViewModel()
+    val showNewQuestionDialog = viewModel.showNewQuestionDialog.collectAsState().value
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -46,6 +54,16 @@ fun NewEvaluationScreen(
                     navigationIconContentColor = MaterialTheme.colorScheme.onPrimary
                 )
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = viewModel::setShowNewQuestionDialog
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.Add,
+                    contentDescription = "New question"
+                )
+            }
         }
     ) { innerPadding ->
         Box(
@@ -58,5 +76,13 @@ fun NewEvaluationScreen(
                 text = "New evaluation screen"
             )
         }
+    }
+
+    if (showNewQuestionDialog) {
+        NewQuestionDialog(
+            viewModel = viewModel,
+            onConfirm = viewModel::onConfirm,
+            onDismiss = viewModel::setShowNewQuestionDialog
+        )
     }
 }
---
+++ app/src/main/java/com/ralphmarondev/swiftech/features/evaluation/presentation/new_evaluation/NewEvaluationViewModel.kt
@@ -0,0 +1,53 @@
+package com.ralphmarondev.swiftech.features.evaluation.presentation.new_evaluation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ralphmarondev.swiftech.core.domain.model.Result
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class NewEvaluationViewModel : ViewModel() {
+
+    /*
+     * title
+     * description
+     * questions
+     */
+    private val _newQuestion = MutableStateFlow("")
+    val newQuestion = _newQuestion.asStateFlow()
+
+    private val _showNewQuestionDialog = MutableStateFlow(false)
+    val showNewQuestionDialog = _showNewQuestionDialog.asStateFlow()
+
+    private val _response = MutableStateFlow<Result?>(null)
+    val response = _response.asStateFlow()
+
+
+    fun onNewQuestionValueChange(value: String) {
+        _newQuestion.value = value
+    }
+
+    fun setShowNewQuestionDialog() {
+        _showNewQuestionDialog.value = !_showNewQuestionDialog.value
+    }
+
+    fun onConfirm() {
+        viewModelScope.launch {
+            val question = newQuestion.value
+
+            if (question.isEmpty()) {
+                _response.value = Result(
+                    success = false,
+                    message = "Question cannot be empty"
+                )
+                return@launch
+            }
+            _response.value = Result(
+                success = true,
+                message = "Question added"
+            )
+            setShowNewQuestionDialog()
+        }
+    }
+}
