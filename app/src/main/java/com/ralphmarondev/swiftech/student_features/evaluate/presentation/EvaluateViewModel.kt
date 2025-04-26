613a04dc179781384d266ff30f5dd77c35228539
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-04-26 23:44:26 +0800

    getting question of evaluation form by id and displaying it

--- app/src/main/java/com/ralphmarondev/swiftech/student_features/evaluate/presentation/EvaluateViewModel.kt
+++ app/src/main/java/com/ralphmarondev/swiftech/student_features/evaluate/presentation/EvaluateViewModel.kt
@@ -4,14 +4,16 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ralphmarondev.swiftech.core.domain.usecases.course.GetCourseDetailByIdUseCase
 import com.ralphmarondev.swiftech.core.domain.usecases.user.GetUserByIdUseCase
+import com.ralphmarondev.swiftech.student_features.evaluate.domain.usecase.GetEvaluationFormQuestionByIdUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class EvaluateViewModel(
-    private val id: Int,
+    private val formId: Int,
     private val getCourseDetailByIdUseCase: GetCourseDetailByIdUseCase,
-    private val getUserByIdUseCase: GetUserByIdUseCase
+    private val getUserByIdUseCase: GetUserByIdUseCase,
+    private val getEvaluationFormQuestionByIdUseCase: GetEvaluationFormQuestionByIdUseCase
 ) : ViewModel() {
 
     private val _courseName = MutableStateFlow("")
@@ -25,19 +27,25 @@ class EvaluateViewModel(
 
     init {
         viewModelScope.launch {
-            val course = getCourseDetailByIdUseCase(id)
-            _courseName.value = course?.name ?: "Course name not defined."
+//            val course = getCourseDetailByIdUseCase(id)
+//            _courseName.value = course?.name ?: "Course name not defined."
+//
+//            _courseTeacher.value = getUserByIdUseCase(course?.teacherId ?: -1)?.fullName
+//                ?: "Teacher name not defined."
 
-            _courseTeacher.value = getUserByIdUseCase(course?.teacherId ?: -1)?.fullName
-                ?: "Teacher name not defined."
+//            _questions.value = listOf(
+//                "Displayed energy, enthusiasm, and commitment to teaching.",
+//                "Arrived promptly and remained for the agreed upon time.",
+//                "Demonstrated ability to learn about students.",
+//                "Demonstrated ability to design lessons based on student's needs.",
+//                "Displayed sensitivity to special needs students."
+//            )
+            _courseName.value = "Tech"
+            _courseTeacher.value = "Ralph"
 
-            _questions.value = listOf(
-                "Displayed energy, enthusiasm, and commitment to teaching.",
-                "Arrived promptly and remained for the agreed upon time.",
-                "Demonstrated ability to learn about students.",
-                "Demonstrated ability to design lessons based on student's needs.",
-                "Displayed sensitivity to special needs students."
-            )
+            getEvaluationFormQuestionByIdUseCase(formId).collect { questions ->
+                _questions.value = questions.map { it.questionText }
+            }
         }
     }
 }
